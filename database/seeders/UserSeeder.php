53f23d3d4c3a1c01eb64c87849a60b623cea9698
ryoadam <ryoadam6@gmail.com>
2025-01-31 23:58:33 +0800

    git init

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,7 +13,7 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\User::factory(20)->create();
+        // \App\Models\User::factory(20)->create();
         \App\Models\User::create([
             'name' => 'Admin Fahiem',
             'email' => 'fahim@gmail.com',
